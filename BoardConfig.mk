e14a0b8e209beac70ca20d4ebabb5a8ebd6f7434
HostZero <haneesh.da.rocker@gmail.com>
2016-06-02 16:46:21 +0530

    Updated According to TWRP Requirements

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,8 +15,65 @@
 #
 
 # inherit from common sprout repo
--include device/google/sprout-common/BoardConfigCommon.mk
 
-TARGET_OTA_ASSERT_DEVICE := sprout,sprout4
+TARGET_OTA_ASSERT_DEVICE := sprout
 
-TARGET_RECOVERY_FSTAB := device/google/sprout4/rootdir/root/fstab.sprout
+TARGET_RECOVERY_FSTAB := device/google/sprout/rootdir/root/twrp.fstab
+
+LOCAL_PATH := device/google/sprout
+
+TARGET_BOARD_PLATFORM := mt6582
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+ARCH_ARM_HAVE_VFP := true
+TARGET_CPU_SMP := true
+TARGET_ARCH := arm
+ARCH_ARM_HAVE_NEON := true
+TARGET_NO_BOOTLOADER := true
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a7
+TARGET_CPU_VARIANT:= cortex-a7
+TARGET_CPU_MEMCPY_OPT_DISABLE := true
+
+# Storage allocations
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00600000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10000000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 956964608
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
+BOARD_CACHEIMAGE_PARTITION_SIZE := 134217728
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# kernel stuff
+TARGET_PREBUILT_KERNEL := device/google/sprout/rootdir/root/kernel
+TARGET_KERNEL_SOURCE := 
+TARGET_KERNEL_CONFIG := 
+BOARD_KERNEL_CMDLINE :=
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x04000000 --tags_offset 0x00000100
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432 #65536 * 512 boot
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33538048 #65504 * 512 recovery
+
+# TWRP stuff
+TW_NO_CPU_TEMP := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_INCLUDE_JB_CRYPTO := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/mtk-msdc.0/by-name/userdata"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data=ordered"
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS := 255
+TW_NO_USB_STORAGE := true
+TW_NO_BATT_PERCENT := false
+TW_NO_REBOOT_BOOTLOADER := false
+TW_NO_REBOOT_RECOVERY := false
+TW_NO_REBOOT_DOWNLOAD := false
+TARGET_USERIMAGES_USE_F2FS := true
+TARGET_USERIMAGES_USE_EXT4 := true
