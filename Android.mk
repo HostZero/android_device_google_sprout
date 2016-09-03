7b8a095e0c66d1dbd9e7fd6170dfaa45ffd4934a
HostZero <haneesh.da.rocker@gmail.com>
2016-09-03 20:57:12 +0530

    spout: build stable

--- Android.mk
+++ Android.mk
@@ -26,7 +26,7 @@
 LOCAL_PATH := $(call my-dir)
 
 # UNOFFICIAL, NIGHTLY, STABLE
-RECOVERY_BUILDTYPE := UNOFFICIAL
+export RECOVERY_BUILDTYPE := STABLE
 
 ifneq ($(filter sprout,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
