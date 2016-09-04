d3078b62334b317f3633e32251163d8b681493b0
HostZero <haneesh.da.rocker@gmail.com>
2016-09-04 21:25:00 +0530

    sprout: generate new fingerprint and build-desc

--- omni_sprout.mk
+++ omni_sprout.mk
@@ -6,8 +6,6 @@ TARGET_SCREEN_WIDTH := 480
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, device/google/sprout/device.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys"
-
 PRODUCT_NAME := omni_sprout
 PRODUCT_DEVICE := sprout
 PRODUCT_BRAND := google
