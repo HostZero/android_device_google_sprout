b95fcdd5d80f5d2680508f13a2bd1ce07c3118aa
HostZero <haneesh.da.rocker@gmail.com>
2016-12-07 18:35:41 +0530

    Remove uneeded stuff
    * Remove uneeded stuff for twrp
    --> Remove Wifi Configration
    --> Remove OpenGL Render
    --> Remove Minkin

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,26 +78,8 @@ TW_HAS_DOWNLOAD_MODE := true
 TW_EXCLUDE_SUPERSU := true
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
 
-# OpenGL
-USE_OPENGL_RENDERER:= true
-
-# WiFi
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_mt66xx
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_mt66xx
-WIFI_DRIVER_FW_PATH_PARAM:="/dev/wmtWifi"
-WIFI_DRIVER_FW_PATH_STA:=STA
-WIFI_DRIVER_FW_PATH_AP:=AP
-WIFI_DRIVER_FW_PATH_STA:=P2P
-
-# Enable Minikin text layout engine (will be the default soon)
-USE_MINIKIN := true
-
 MALLOC_SVELTE := true
 DEVICE_RESOLUTION := 480x854
-
 
 # Fonts
 EXTENDED_FONT_FOOTPRINT := true
